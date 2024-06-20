1c0809a8f48c196ae176fde022ba8a503522acae
Juliusz Maretzky <kontakt@juliuszmaretzky.pl>
2024-06-20 20:47:55 +0200

    Tests v2

---
+++ PlayWrightAutomationTests/PlaywrightPageTest.cs
@@ -0,0 +1,93 @@
+﻿//https://www.youtube.com/watch?v=d31_UcGIac8&list=PLyR3u3h9srduMQ0G2SefuMz3Dkkkzs59k&index=2
+
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PlayWrightAutomationTests
+{
+    [TestFixture]
+    public class PlaywrightPageTest : PageTest
+    {
+        [Test]
+        public async Task Login_PageTest_TC001()
+        {
+            await Page.GotoAsync("https://adactinhotelapp.com/");
+            await Expect(Page).ToHaveTitleAsync(new Regex("Adactin.com - Hotel Reservation System"));
+            await Page.FillAsync("#username", "Pepeusz123");
+            await Page.FillAsync("#password", "pepe123_P");
+            await Page.ClickAsync("#login");
+            await Expect(Page).ToHaveTitleAsync(new Regex("Adactin.com - Search Hotel"));
+
+            var locator = Page.Locator(".welcome_menu").First;
+            await Assertions.Expect(locator).ToHaveTextAsync(new Regex("Welcome to Adactin Group of Hotels"));
+
+            await Page.CloseAsync();
+        }
+
+        [Test]
+        public async Task GetByPlaceHolder()
+        {
+            await Page.GotoAsync("https://demoqa.com/text-box");
+            await Page.GetByPlaceholder("Full Name").FillAsync("Tester1");
+            await Page.GetByPlaceholder("name@example.com").FillAsync("pepe@pepe.pepe");
+            await Page.GetByPlaceholder("Current Address").FillAsync("Pepe 12/3");
+        }
+
+        [Test]
+        public async Task GetByText()
+        {
+            await Page.GotoAsync("https://demoqa.com/links");
+            await Page.GetByText("Created").ClickAsync();
+        }
+
+        [Test]
+        public async Task GetByAltText()
+        {
+            await Page.GotoAsync("https://playwright.dev/dotnet/docs/locators#locate-by-alt-text");
+            await Page.GetByAltText("playwright logo").First.ClickAsync();
+        }
+
+        [Test]
+        public async Task GetByLabel()
+        {
+            await Page.GotoAsync("https://playwright.dev/dotnet/docs/locators#locate-by-title");
+            await Page.GetByLabel("Password").FillAsync("secret112233");
+            Thread.Sleep(5000);
+        }
+
+        [Test]
+        public async Task GetByTitle()
+        {
+            await Page.GotoAsync("https://playwright.dev/dotnet/docs/locators#locate-by-title");
+            await Expect(Page.GetByTitle("Issues count")).ToHaveTextAsync("25 issues");
+        }
+
+        [Test]
+        public async Task GetByRole()
+        {
+            await Page.GotoAsync("https://playwright.dev/dotnet/docs/locators#locate-by-role");
+            await Page.SetViewportSizeAsync(width: 1920, height: 1080);
+
+            await Expect(Page
+                .GetByRole(AriaRole.Heading, new() { Name = "Sign up"}))
+                .ToBeVisibleAsync();
+
+            await Page
+                .GetByRole(AriaRole.Checkbox, new() { Name = "Subscribe" })
+                .CheckAsync();
+
+            await Page
+                .GetByRole(AriaRole.Button, new()
+                {
+                    NameRegex = new Regex("submit", RegexOptions.IgnoreCase)
+                })
+                .ClickAsync();
+        }
+    }
+}
--- PlayWrightAutomationTests/TestsFromYT.cs
+++ PlayWrightAutomationTests/TestsFromYT.cs
@@ -1,3 +1,5 @@
+//https://www.youtube.com/watch?v=d31_UcGIac8&list=PLyR3u3h9srduMQ0G2SefuMz3Dkkkzs59k&index=2
+
 using Microsoft.Playwright;
 using System.Text.RegularExpressions;
 
@@ -18,6 +20,7 @@ namespace PlayWrightAutomationTests
                 });
             var context = await browser.NewContextAsync();
             var page = await context.NewPageAsync();
+
             await page.GotoAsync("https://adactinhotelapp.com");
             await page.FillAsync("#username", "Pepeusz123");
             await page.FillAsync("#password", "pepe123_P");
@@ -41,6 +44,7 @@ namespace PlayWrightAutomationTests
                 });
             var context = await browser.NewContextAsync();
             var page = await context.NewPageAsync();
+
             await page.GotoAsync("https://adactinhotelapp.com");
             await page.Locator("#username").FillAsync("Pepeusz123");
             await page.Locator("#password").FillAsync("pepe123_P");
@@ -65,6 +69,7 @@ namespace PlayWrightAutomationTests
                 });
             var context = await browser.NewContextAsync();
             var page = await context.NewPageAsync();
+
             await page.GotoAsync("https://adactinhotelapp.com");
             await page.Locator("#username").FillAsync("Pepeusz123");
             await page.Locator("#password").FillAsync("pepe123_P");
@@ -92,6 +97,7 @@ namespace PlayWrightAutomationTests
                     ViewportSize = new ViewportSize() { Width = 1920, Height = 1080 }
                 });
             var page = await context.NewPageAsync();
+
             await page.GotoAsync("https://adactinhotelapp.com");
             await page.Locator("#username").FillAsync("Pepeusz123");
             await page.Locator("#password").FillAsync("pepe123_P");
@@ -100,6 +106,135 @@ namespace PlayWrightAutomationTests
             await Assertions.Expect(locator).ToHaveTextAsync(
                 new Regex("Welcome to Adactin Group of Hotels"));
             await page.CloseAsync();
+        }
+
+        [Test]
+        public async Task Trace_TC005()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 1
+            });
+            var context = await browser.NewContextAsync();
+
+            await context.Tracing.StartAsync(new()
+            {
+                Screenshots = true,
+                Snapshots = true, 
+                Sources = true
+            });
+
+            var page = await context.NewPageAsync();
+            await page.SetViewportSizeAsync(1920, 1080);
+
+            await page.GotoAsync("https://adactinhotelapp.com/");
+            await page.FillAsync("#username", "Pepeusz123");
+            await page.FillAsync("#password", "pepe123_P");
+            await page.ClickAsync("#login");
+
+            await page.TypeAsync("#location", "Sydney");
+            await page.ClickAsync("#Submit");
+            await page.ClickAsync("#radiobutton_1");
+            await page.ClickAsync("#continue");
+
+            await context.Tracing.StopAsync(new()
+            {
+                Path = "trace/trace.zip"
+            });
+            await context.CloseAsync();
+            await browser.CloseAsync();
+            //you can view trace by powershell: cd [project path\bin\Debug\net6.0]
+            //.playwright.ps1 show-trace "[project path\bin\Debug\net6.0\trace\trace.zip]"
+            //or drag'n'drop trace.zip into https://trace.playwright.dev
+        }
+
+        [Test]
+        public async Task SaveState_TC006()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 1
+            });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://adactinhotelapp.com/");
+            await page.FillAsync("#username", "Pepeusz123");
+            await page.FillAsync("#password", "pepe123_P");
+            await page.ClickAsync("#login");
+
+            await context.StorageStateAsync(new()
+            {
+                Path = @"state\state_login.json"
+            });
+
+            await context.CloseAsync();
+            await browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task RetrieveState_TC006()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 1
+            });
+            var context = await browser.NewContextAsync(new()
+            {
+                StorageStatePath = @"state\state_login.json"
+            });
+
+            var page = await context.NewPageAsync();
+
+            Thread.Sleep(3000);
+
+            await page.GotoAsync("https://adactinhotelapp.com/SearchHotel.php");
+            await page.TypeAsync("#location", "Sydney");
+
+            await page.GotoAsync("https://adactinhotelapp.com/SelectHotel.php");
+            await page.ClickAsync("#continue");
+        }
+
+        [Test]
+        public async Task Login_CodeGen_TC007()
+        {
+            //in powershell: .\playwright.ps1 codegen https://adactinhotelapp.com/
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://adactinhotelapp.com/");
+            await page.Locator("#username").FillAsync("Pepeusz123");
+            await page.Locator("#password").FillAsync("pepe123_P");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            await page.Locator("#location").SelectOptionAsync(new[] { "Sydney" });
+            await page.GetByRole(AriaRole.Button, new() { Name = "Search" }).ClickAsync();
+            await page.Locator("#radiobutton_2").CheckAsync();
+            await page.GetByRole(AriaRole.Button, new() { Name = "Continue" }).ClickAsync();
+            await page.Locator("#first_name").ClickAsync();
+            await page.Locator("#first_name").FillAsync("Pepeusz");
+            await page.Locator("#first_name").PressAsync("Tab");
+            await page.Locator("#last_name").FillAsync("Pepeuszowski");
+            await page.Locator("#address").ClickAsync();
+            await page.Locator("#address").FillAsync("Pepe Address 12/3");
+            await page.Locator("#cc_num").ClickAsync();
+            await page.Locator("#cc_num").FillAsync("7418529630789512");
+            await page.Locator("#cc_type").SelectOptionAsync(new[] { "AMEX" });
+            await page.Locator("#cc_exp_month").SelectOptionAsync(new[] { "2" });
+            await page.Locator("#cc_exp_year").SelectOptionAsync(new[] { "2025" });
+            await page.Locator("#cc_cvv").ClickAsync();
+            await page.Locator("#cc_cvv").FillAsync("123");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Book Now" }).ClickAsync();
         }
     }
 }
