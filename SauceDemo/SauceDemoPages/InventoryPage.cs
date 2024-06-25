d483b7e47158d86c7ea0a0c6eff37874b56a0bf7
Juliusz Maretzky <kontakt@juliuszmaretzky.pl>
2024-06-25 22:02:37 +0200

    edit test name: Check_If_Product_Sorting_From_Z_To_A_Works_Properly and check product names order due to sorting

--- SauceDemo/SauceDemoPages/InventoryPage.cs
+++ SauceDemo/SauceDemoPages/InventoryPage.cs
@@ -12,17 +12,20 @@ namespace SauceDemo.SauceDemoPages
         private const string pageAddress = "https://www.saucedemo.com/inventory.html";
         private readonly IPage _page;
         private readonly ILocator pageHeader;
-        private readonly ILocator productName;
         private readonly ILocator productSortDropDown;
-        public readonly Dictionary<string, string> productSortOptions;
+        private readonly ILocator activeSortOption;
 
+        public readonly string productNameXPath;
+        public readonly Dictionary<string, string> productSortOptions;
 
         public InventoryPage(IPage page)
         {
             _page = page;
             pageHeader = _page.Locator("//*[@class='app_logo']");
-            productName = _page.Locator("//*[@class='inventory_item_name ']");
             productSortDropDown = _page.Locator("//*[@class='product_sort_container']");
+            activeSortOption = _page.Locator("//*[@class='active_option']");
+
+            productNameXPath = "//*[contains(@class,'inventory_item_name')]";
             productSortOptions = new Dictionary<string, string>() 
             {
                 ["Name (A to Z)"] = "az",
@@ -35,6 +38,7 @@ namespace SauceDemo.SauceDemoPages
         public async Task WaitForLoad()
         {
             await Assertions.Expect(pageHeader).ToHaveTextAsync("Swag Labs");
+            await Assertions.Expect(activeSortOption).ToHaveTextAsync("Name (A to Z)");
         }
 
         public async Task SortProducts(string sortBy)
--- SauceDemo/SauceDemoTests/InventoryTests.cs
+++ SauceDemo/SauceDemoTests/InventoryTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Playwright;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,33 @@ namespace SauceDemo.SauceDemoTests
         private const string standardPassword = "secret_sauce";
         private const string standardUsername = "standard_user";
 
-        [Test]
-        public async Task Check_If_Product_Sorting_Works_Properly()
+        private async Task AssertProductNamesOrder(List<string> productNames)
         {
+            for (int i = 0; i < productNames.Count; i++)
+            {
+                await Assertions.Expect(Page.Locator($"({InventoryPage.productNameXPath})[{i + 1}]")).ToHaveTextAsync(productNames[i]);
+            }
+        }
+
+        [Test]
+        public async Task Check_If_Product_Sorting_From_Z_To_A_Works_Properly()
+        {
+            var productNamesList = new List<string>() { 
+                "Sauce Labs Backpack",
+                "Sauce Labs Bike Light",
+                "Sauce Labs Bolt T-Shirt",
+                "Sauce Labs Fleece Jacket",
+                "Sauce Labs Onesie",
+                "Test.allTheThings() T-Shirt (Red)"
+            };
             await LoginPage.GoToLoginPage();
             await LoginPage.FillCredentials(standardUsername, standardPassword);
             await LoginPage.ClickLogin();
             await InventoryPage.WaitForLoad();
+            await AssertProductNamesOrder(productNamesList);
             await InventoryPage.SortProducts(InventoryPage.productSortOptions["Name (Z to A)"]);
+            productNamesList.Reverse();
+            await AssertProductNamesOrder(productNamesList);
         }
     }
 }
